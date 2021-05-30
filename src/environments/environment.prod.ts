0fac1d4d8b6ca0573c93f6351b7ed69953616978
Szekeres Laura <h880629@stud.u-szeged.hu>
2021-05-30 15:41:47 +0200

    lastcommit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyCPmgja-UJozAPd1m7siO3D9ZsHQK3CIYI",
+    authDomain: "agreementmanagement-ab98a.firebaseapp.com",
+    projectId: "agreementmanagement-ab98a",
+    storageBucket: "agreementmanagement-ab98a.appspot.com",
+    messagingSenderId: "604428062775",
+    appId: "1:604428062775:web:74f14cdbdb3f6dfeabafb2",
+    measurementId: "G-NVH7TJXHQP"
+  },
   production: true
 };
